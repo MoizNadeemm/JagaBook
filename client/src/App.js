c63175ff53ef5245829b1b0f51eaf90b54242d70
Moiz Nadeem <mabdulmoiz82@gmail.com>
2025-12-14 15:10:04 +0500

    Initial commit with Home and Login pages

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <h1>JagaBook</h1>
+      <Home />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Browse verified photoshoot places in Pakistan.</p>
+    </div>
+  );
+}
+
+export default Home;
